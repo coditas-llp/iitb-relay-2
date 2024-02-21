ab292caff2fd3ac97c54d48b554e1c9c921c1748
akhileshmagdum-coditas <akhilesh.magdum@coditas.com>
2024-02-21 07:36:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/main/java/com/iitb/iitbrelay/IitbRelayApplication.java
@@ -0,0 +1,13 @@
+package com.iitb.iitbrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IitbRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IitbRelayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iitb/iitbrelay/IitbRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitb.iitbrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IitbRelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
